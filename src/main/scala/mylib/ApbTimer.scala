b7fe599bac797e34ef53cdf0b1f0a7100542a2d7
Tom Verbeure <hombre+github@gmail.com>
2018-08-11 23:23:53 -0700

    Single bit input tick

--- src/main/scala/mylib/ApbTimer.scala
+++ src/main/scala/mylib/ApbTimer.scala
@@ -3,21 +3,21 @@ package mylib
 
 import spinal.core._
 import spinal.lib._
-import spinal.lib.bus.amba3.apb.{Apb3, Apb3SlaveFactory}
 import spinal.lib.bus.amba3.apb._
 import spinal.lib.misc.InterruptCtrl
 
 case class ApbTimer(width : Int) extends Component{
   val io = new Bundle{
     val apb   = slave(Apb3(Apb3Config(addressWidth = 8, dataWidth = 32)))
-    val ticks = in Bits(width bits)
+    val tick  = in Bool
   }
 
   val timerA = Timer(width = 32)
   val busCtrl = Apb3SlaveFactory(io.apb)
 
   val timerABridge = timerA.driveFrom(busCtrl,0x40)(
-     ticks  = List(io.ticks.orR),
+    // The 'True' allows for a mode where the timer increments each cycle without the need for activity on io.tick
+    ticks  = Seq(True, io.tick),
     // By looping the timer full to the clears, it allows you to create an autoreload mode.
     clears = List(timerA.io.full)
   )
